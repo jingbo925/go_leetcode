fb5f0ab9c2962033aaccbc5fcd101106432ac00e
jingbo <jingbo8886@gmail.com>
2020-11-19 22:59:36 +0800

    0003

--- Algorithms/0003.Longest-Substring-Without-Repeating-Characters/0003.Longest-Substring-Without-Repeating-Characters.go
+++ Algorithms/0003.Longest-Substring-Without-Repeating-Characters/0003.Longest-Substring-Without-Repeating-Characters.go
@@ -1,16 +1,11 @@
 package main
 
-import (
-	"fmt"
-)
-
 func lengthOfLongestSubstring(s string) int {
 	lastOccurred := make(map[byte]int)
 	start := 0
 	maxLength := 0
 
 	for i, ch := range []byte(s) {
-		fmt.Println(ch, i)
 		if lastI, ok := lastOccurred[ch]; ok && lastI >= start {
 			start = lastOccurred[ch] + 1
 		}
