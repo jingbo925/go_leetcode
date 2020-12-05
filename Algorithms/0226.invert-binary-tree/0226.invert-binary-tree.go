9e45f1ec972a6cbfc53040c0be40ec2a23b4960a
jingbo <jingbo8886@gmail.com>
2020-12-05 20:10:24 +0800

    226

---
+++ Algorithms/0226.invert-binary-tree/0226.invert-binary-tree.go
@@ -0,0 +1,19 @@
+package tree
+
+/**
+ * Definition for a binary tree node.
+ * type TreeNode struct {
+ *     Val int
+ *     Left *TreeNode
+ *     Right *TreeNode
+ * }
+ */
+func invertTree(root *TreeNode) *TreeNode {
+	if root == nil {
+		return root
+	}
+	invertTree(root.Left)
+	invertTree(root.Right)
+	root.Left, root.Right = root.Right, root.Left
+	return root
+}
