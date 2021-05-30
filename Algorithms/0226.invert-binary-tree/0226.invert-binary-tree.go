419573e7dbb3916626114a364378dcc2594fee84
jingbo <jingbo8886@gmail.com>
2021-05-30 21:44:58 +0800

    fix: uadd 49

--- Algorithms/0226.invert-binary-tree/0226.invert-binary-tree.go
+++ Algorithms/0226.invert-binary-tree/0226.invert-binary-tree.go
@@ -8,12 +8,4 @@ package tree
  *     Right *TreeNode
  * }
  */
-func invertTree(root *TreeNode) *TreeNode {
-	if root == nil {
-		return root
-	}
-	invertTree(root.Left)
-	invertTree(root.Right)
-	root.Left, root.Right = root.Right, root.Left
-	return root
-}
+c
